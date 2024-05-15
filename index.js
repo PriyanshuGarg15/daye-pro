6536bcdb39a07fe5a5cfcf8630701be17f586418
priyanshuG15 <priyanshu.garg@rupyy.com>
2024-05-15 13:31:32 +0530

    first commit, server up

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const rateLimitMiddleware = require('./middleware/rateLimitMiddleware');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: 'config/config.env' });
+}
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Error handling for uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+});
+
+// Apply rate limit middleware
+app.use(rateLimitMiddleware);
+
+// Enable CORS
+app.use(cors());
+
+// Parse JSON requests
+app.use(express.json());
+
+// Parse URL-encoded requests
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Error handler for rate limit exceeded errors
+app.use((err, req, res, next) => {
+    if (err && err.message === 'Too many requests from this IP, please try again later') {
+        res.status(429).json({ error: 'Rate limit exceeded' });
+    } else {
+        next(err);
+    }
+});
+
+//Product Creation Route
+// const product = require('./routes/productRoute');
+// app.use('/api', product)
+
+// Health check endpoint
+app.get('/healthCheck', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
